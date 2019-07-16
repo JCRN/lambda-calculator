05611108a8cebb86c2f1800acc2c9a6d89ec1b06
JCRN <clemons.jason@gmail.com>
2019-07-16 11:40:57 -0700

    ButtonComponents: added import statements for each button type, finalized state for each button type, mapped state data to create button components for each button type

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 
 //import any components needed
-
+import OperatorButton from "./OperatorButton.js";
 //Import your array data to from the provided data file
 import { operators } from "../../../data.js";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
-  const [operator, setOperator] = useState();
+  const [operators, setOperators] = useState(operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+      operators.map(operator => (
+        <OperatorButton operator={operator} />
+      ))}
     </div>
   );
 };
